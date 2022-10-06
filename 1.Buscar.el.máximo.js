e0e341832fe5e763dffd6614a73d624e94047153
Raul <raul2851@gmail.com>
2022-10-06 13:02:12 +0200

    ejercicios

---
+++ 1.Buscar.el.máximo.js
@@ -0,0 +1,22 @@
+//1: Buscar el máximo
+
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+//function sum(numberOne , numberTwo) {
+    // insert code
+//  }
+
+const number = "numberOne, numberTwo";
+
+function sum(numberOne, numberTwo) {
+  let num1 = 1;
+  let num2 = 2;
+    if (num1 > num2) {
+        return num1;
+      } else {
+        return num2;
+      }
+  }
+
+  console.log(sum("numberone, numberTwo"));
+  
